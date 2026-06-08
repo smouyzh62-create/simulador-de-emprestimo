de3801bb623079ca9ecdd68afdbb8db9181c9ff0
smouyzh62-create <smouyzh62-create@users.noreply.github.com>
2026-06-09 02:50:46 +0800

    Fix main.tsx import to use Admin (uppercase)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import Admin from './admin';
+import Admin from './Admin';
 import './index.css';
 
 const isAdmin = window.location.pathname.startsWith('/admin');
