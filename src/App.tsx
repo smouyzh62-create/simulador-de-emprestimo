7bd1bc79cc8a2ac2c69ddc07aa1d8cf8abd51e08
smouyzh62-create <smouyzh62-create@users.noreply.github.com>
2026-06-09 02:34:25 +0800

    Ensure FB Pixel section persists in Admin.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -58,26 +58,26 @@ export default function App() {
   const [showSecurityModal, setShowSecurityModal] = useState(false);
   const [showPrivacyModal, setShowPrivacyModal] = useState(false);
 
-  // Load Facebook Pixel from admin config
-  useEffect(() => {
-    const pixelId = (() => {
-      try { return localStorage.getItem('bb_fb_pixel_id'); } catch { return null; }
-    })();
-    if (!pixelId) return;
-    const noscript = document.createElement('noscript');
-    noscript.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=' + pixelId + '&ev=PageView&noscript=1"/>';
-    document.head.appendChild(noscript);
-    const w = window as any;
-    (function(f,b,e,v,n,t,s){
-      if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)
-    })(w,document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    w.fbq('init', pixelId);
-    w.fbq('track', 'PageView');
-  }, []);
+ // Load Facebook Pixel from admin config
+ useEffect(() => {
+   const pixelId = (() => {
+     try { return localStorage.getItem('bb_fb_pixel_id'); } catch { return null; }
+   })();
+   if (!pixelId) return;
+   const noscript = document.createElement('noscript');
+   noscript.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=' + pixelId + '&ev=PageView&noscript=1"/>';
+   document.head.appendChild(noscript);
+   const w = window as any;
+   (function(f,b,e,v,n,t,s){
+     if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+     if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+     n.queue=[];t=b.createElement(e);t.async=!0;
+     t.src=v;s=b.getElementsByTagName(e)[0];
+     s.parentNode.insertBefore(t,s)
+   })(w,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+   w.fbq('init', pixelId);
+   w.fbq('track', 'PageView');
+ }, []);
 
   // Processing Animation state
   const [progress, setProgress] = useState(0);
